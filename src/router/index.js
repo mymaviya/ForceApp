6a8a45ba0ca07979817a9f636e49d52b6a564518
mudassir <mhmasti@gmail.com>
2021-02-06 14:52:25 +0530

    set admin route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
 import Admin from "../views/Admin.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Calendar from "../views/Calendar.vue";
@@ -10,8 +10,8 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "admin",
+    component: Admin
   },
   {
     path: "/admin",
